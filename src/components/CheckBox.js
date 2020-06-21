921e79cbe61c7cecfb2ab45c149f422a5d0f2e00
cuhpepcekr <lintogm@gmail.com>
2020-06-21 20:01:23 +0900

    CSS Module - use react-icons

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { MdCheckBox, MdCheckBoxOutlineBlank } from "react-icons/md";
 
 function CheckBox({ checked, children, ...rest }) {
   return (
     <div>
       <label>
         <input type="checkbox" checked={checked} {...rest} />
-        <div>{checked ? "체트됨" : "체크 안 됨"}</div>
+        <div>{checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}</div>
       </label>
       <span> {children}</span>
     </div>
